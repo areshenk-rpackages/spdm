93f97f1c60f1e71094b28476e89454de7d2a5cb1
areshenk <areshenk@protonmail.com>
2020-11-06 10:54:50 -0500

    Include bold data

---
+++ R/data-documentation.R
@@ -0,0 +1,16 @@
+#' FMRI BOLD data
+#'
+#' Dataset contains fMRI BOLD data from from five subjects in each of five
+#' resting-state conditions. Object is a data frame with the following fields:
+#' \itemize{
+#'   \item{"Subject"} Subject ID
+#'   \item{"Condition"} Condition label
+#'   \item{"Data"} A 150 x 5 matrix containing the standardized BOLD signal
+#'   from 5 cortical regions.
+#' }
+#'
+#' @name bold
+#' @docType data
+#' @author Corson N. Areshenkoff \email{areshenk@protonmail.com}
+#' @keywords data
+NULL
